1df0a01a8e815e4568a75c71cd57d0999ba85555
evi0s <i@evi0s.com>
2025-02-26 12:40:28 -0500

    [version] add new version 13331

--- frida/hook.js
+++ frida/hook.js
@@ -1,11 +1,14 @@
 const VERBOSE = false;
 
-const getMainModule = () => {
+const getMainModule = (version) => {
+    if (version >= 13331) {
+        return Process.findModuleByName("flue.dll");
+    }
     return Process.findModuleByName("WeChatAppEx.exe");
 }
 
 
-const patchResourceCachePolicy = (base, offset) => {
+const patchResourceCachePolicy = (base, offset, version) => {
     // xref: WAPCAdapterAppIndex.js
     Interceptor.attach(base.add(offset), {
         onEnter(args) {
@@ -18,7 +21,7 @@ const patchResourceCachePolicy = (base, offset) => {
     });
 }
 
-const patchCDPFilter = (base, offset) => {
+const patchCDPFilter = (base, offset, version) => {
     // filter function: sub_14342D970
     // xref: SendToClientFilter OR devtools_message_filter_applet_webview.cc
     Interceptor.attach(base.add(offset), {
@@ -36,9 +39,13 @@ const patchCDPFilter = (base, offset) => {
     });
 }
 
-const onLoadStartHook = (a1, a2) => {
-    const passArgs = a1.add(56).readPointer().add(1208).readPointer();
-    const passConditionPtr = passArgs.add(8).readPointer().add(1160).readPointer().add(16).readPointer().add(488);
+const onLoadStartHook = (a1, a2, version) => {
+    let structOffset = [1208, 1160, 16, 488];
+    if (version === 13331) {
+        structOffset = [1272, 1224, 16, 488];
+    }
+    const passArgs = a1.add(56).readPointer().add(structOffset[0]).readPointer();
+    const passConditionPtr = passArgs.add(8).readPointer().add(structOffset[1]).readPointer().add(structOffset[2]).readPointer().add(structOffset[3]);
     VERBOSE && console.log("[hook] scene:", passConditionPtr.readInt());
     const sceneNumberArray = [1256];
     if (!sceneNumberArray.includes(passConditionPtr.readInt())) {
@@ -53,7 +60,7 @@ const onLoadStartHook = (a1, a2) => {
     // websocketServerStringPtr.writeUtf8String("ws://127.0.0.1:8189/");
 }
 
-const interceptor = (base, offset) => {
+const interceptor = (base, offset, version) => {
     // xref: AppletIndexContainer::OnLoadStart
     Interceptor.attach(base.add(offset), {
         onEnter(args) {
@@ -63,7 +70,7 @@ const interceptor = (base, offset) => {
                 this.context.rdx = (this.context.rdx & ~0xFF) | 0x1;
             }
             // handle others
-            onLoadStartHook(this.context.rcx, this.context.rdx);
+            onLoadStartHook(this.context.rcx, this.context.rdx, version);
         },
         onLeave(retval) {
             // do nothing
@@ -76,21 +83,21 @@ const parseConfig = () => {
     if (rawConfig.includes("@@")) {
         // test addresses
         return {
-            Version: 11633,
-            LoadStartHookOffset: "0x28F22A0",
-            CDPFilterHookOffset: "0x38D41E0",
-            ResourceCachePolicyHookOffset: "0x294DA80"
+            Version: 13331,
+            LoadStartHookOffset: "0x0FFC200",
+            CDPFilterHookOffset: "0x2420100",
+            ResourceCachePolicyHookOffset: "0x1050590"
         }
     }
     return JSON.parse(rawConfig);
 }
 
 const main = () => {
-    const mainModule = getMainModule();
     const config = parseConfig();
-    interceptor(mainModule.base, config.LoadStartHookOffset);
-    patchResourceCachePolicy(mainModule.base, config.ResourceCachePolicyHookOffset);
-    patchCDPFilter(mainModule.base, config.CDPFilterHookOffset);
+    const mainModule = getMainModule(config.Version);
+    interceptor(mainModule.base, config.LoadStartHookOffset, config.Version);
+    patchResourceCachePolicy(mainModule.base, config.ResourceCachePolicyHookOffset, config.Version);
+    patchCDPFilter(mainModule.base, config.CDPFilterHookOffset, config.Version);
 }
 
 main();
